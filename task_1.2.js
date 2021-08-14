7a3da70501d0760512cb2ebdd39ba3abe0369585
Idel Yusupov <idel84@mail.ru>
2021-08-14 14:20:19 +0300

    lesson_1

---
+++ task_1.2.js
@@ -0,0 +1,8 @@
+'use strict';
+/* Объявить две переменные: admin и name. Записать в name строку "Василий";
+Скопировать значение из name в admin.
+Вывести admin (должно вывестись «Василий»). */
+
+const name = 'Василий';
+const admin = name;
+alert(admin);
